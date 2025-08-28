db669e3dfd6f4d67d1654f84a65a30eca8799c01
Viktor Shyule <tikitviki@gmail.com>
2025-08-28 15:43:04 +0300

    edit script

--- scriptbash.sh
+++ scriptbash.sh
@@ -3,10 +3,10 @@
 # Путь к файлу журнала
 LOG_FILE="/var/log/monitoring.log"
 
-# Проверяем наличие запущенного процесса 'test'
-if pgrep -x "test" > /dev/null; then
+# Проверяем наличие запущенного процесса 'NetworkManager'
+if pgrep -x "NetworkManager" > /dev/null; then
     # Процесс найден, отправляем HTTPS-запрос на мониторинговый API
-    if curl --output /dev/null --silent --head --fail "https://test.com/monitoring/test/api"; then
+    if curl --output /dev/null --silent --head --fail "https://search.worldbank.org/api/v3/wds?format=json&qterm=energy&display_title=water&fl=display_title&rows=2&os=20"; then
         echo "$(date +'%Y-%m-%d %H:%M:%S') Process is running and server responded successfully." >> "${LOG_FILE}"
     else
         echo "$(date +'%Y-%m-%d %H:%M:%S') Server not available." >> "${LOG_FILE}"
@@ -14,7 +14,6 @@ if pgrep -x "test" > /dev/null; then
 else
     # Процесс не найден, проверяем, был ли он недавно перезагружен
     LAST_RESTART=$(tail -n 1 "${LOG_FILE}" | grep "Process restarted")
-    
     if [[ ! -z ${LAST_RESTART} ]]; then
         echo "$(date +'%Y-%m-%d %H:%M:%S') Process was recently restarted." >> "${LOG_FILE}"
     fi
