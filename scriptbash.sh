f6b7868fa1c052b8af0ce031f4c077de68186bb0
Viktor Shyule <tikitviki@gmail.com>
2025-08-29 17:01:57 +0300

    "Edit bash script: monitor.sh"

--- scriptbash.sh
+++ scriptbash.sh
@@ -2,19 +2,22 @@
 
 # Путь к файлу журнала
 LOG_FILE="/var/log/monitoring.log"
-
+URL="https://search.worldbank.org/api/v3/wds?format=json&qterm=energy&display_title=water&fl=display_title&rows=2&os=20"
+STATUS=$(curl -o /dev/null -s -w "%{http_code}" "$URL")
 # Проверяем наличие запущенного процесса 'NetworkManager'
 if pgrep -x "NetworkManager" > /dev/null; then
     # Процесс найден, отправляем HTTPS-запрос на мониторинговый API
-    if curl --output /dev/null --silent --head --fail "https://search.worldbank.org/api/v3/wds?format=json&qterm=energy&display_title=water&fl=display_title&rows=2&os=20"; then
+    if [ "$STATUS" -eq 200 ] || [ "$STATUS" -eq 300 ]; then
         echo "$(date +'%Y-%m-%d %H:%M:%S') Process is running and server responded successfully." >> "${LOG_FILE}"
     else
         echo "$(date +'%Y-%m-%d %H:%M:%S') Server not available." >> "${LOG_FILE}"
     fi
 else
     # Процесс не найден, проверяем, был ли он недавно перезагружен
-    LAST_RESTART=$(tail -n 1 "${LOG_FILE}" | grep "Process restarted")
-    if [[ ! -z ${LAST_RESTART} ]]; then
+    LAST_RESTART=$(journalctl -e | grep "restart NetworkManager")
+    if [[ ! -z "${LAST_RESTART}" ]]; then
         echo "$(date +'%Y-%m-%d %H:%M:%S') Process was recently restarted." >> "${LOG_FILE}"
+    else
+        echo "$(date +'%Y-%m-%d %H:%M:%S') There is no process." >> "${LOG_FILE}"
     fi
 fi
